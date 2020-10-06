c12c8a6393bfdf11ab1d2fcd7af5919dbae48008
Matthew <savageguy@gmail.com>
2020-10-06 17:11:13 +1300

    Update for github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  router: {
+    base: '/Image-Gallery-Site/'
+  },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
